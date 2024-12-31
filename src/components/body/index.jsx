c76e2972ed97fee851d6e8ce0e38e5925e08cb13
Eduardo Ezequiel Gomez <edu.ezequielgomez@gmail.com>
2024-12-30 21:43:15 -0300

    Cambio imagen

--- src/components/body/index.jsx
+++ src/components/body/index.jsx
@@ -10,7 +10,7 @@ function Body() {
         <div className='container_texto'>
           <p className='frase_imagen'>`La verdadera felicidad está en aceptarse uno mismo y entrar en contacto con uno mismo`</p>
         </div>
-        <img className='imagen_body' src="./public/img1.jpg" alt="" />
+        <img className='imagen_body' src="./public/img1b_1.jpg" alt="" />
       </div>
       <section className='container_comentario'>
           <p className='texto1'>Me alegra mucho que hayas llegado a mi Sitio Web</p>
